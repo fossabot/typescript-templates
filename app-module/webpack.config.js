6b386319707ac4b9546c2c05e7eb99f1e6a84ddb
Adrian Godong <adrian.godong@gmail.com>
2019-06-03 22:30:57 -0700

    Use source-map-loader to make sure library sourcemap gets bundled.

--- app-module/webpack.config.js
+++ app-module/webpack.config.js
@@ -16,6 +16,12 @@ var baseConfig = {
                     }
                 ],
             },
+            // All output '.js' files will have any sourcemaps re-processed by 'source-map-loader'.
+            {
+                enforce: "pre",
+                test: /\.js$/,
+                loader: "source-map-loader"
+            },
         ]
     },
     resolve: { extensions: [".js", ".ts", ".tsx"] },
